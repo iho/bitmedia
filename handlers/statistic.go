284645dae2239743df39696c078fd5fcedece943
Ihor Horobets <ihor@horobets.me>
2020-03-04 14:47:03 +0200

    Add games stats handler

--- handlers/statistic.go
+++ handlers/statistic.go
@@ -12,26 +12,60 @@ import (
 )
 
 type GameStatsQueryParams struct {
-	GameType     string
-	Skip         int
-	Limit        int
-	CreatedStart time.Time `time_format:"2006-01-02"`
-	CreatedEnd   time.Time `time_format:"2006-01-02"`
+	GameType  string
+	Skip      int64
+	Limit     int64
+	DateStart time.Time `time_format:"2006-01-02"`
+	DateEnd   time.Time `time_format:"2006-01-02"`
 }
 
 func (e *Env) GameStats(c *gin.Context) {
-	// usersCollection := e.Db.Collection("user_games")
-	// ctx := c.Request.Context()
+
+	ctx := c.Request.Context()
 	var params GameStatsQueryParams
 	err := c.ShouldBindWith(&params, binding.Query)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": "error"})
 	}
+	var limit int64 = 100
+	if params.Limit != 0 {
+		limit = params.Limit
+	}
+	options := options.Find()
+	options.SetLimit(limit)
+	options.SetSkip(params.Skip)
+	options.SetSort(bson.D{bson.E{Key: "date", Value: 1}, bson.E{Key: "gametype", Value: 1}})
+
+	dbParams := bson.M{}
+
+	if params.GameType != "" {
+		dbParams["game_type"] = params.GameType
+	}
+
+	createdDate := bson.M{}
+	if !params.DateEnd.IsZero() {
+		createdDate["$lte"] = params.DateEnd
+	}
+
+	if !params.DateStart.IsZero() {
+		createdDate["$gt"] = params.DateStart
+	}
+	if len(createdDate) != 0 {
+		dbParams["date"] = createdDate
+	}
+	userStats := e.Db.Collection("stats")
+	cur, err := userStats.Find(ctx, dbParams, options)
 	if err != nil {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	}
-	c.JSON(http.StatusOK, gin.H{"res": err.Error()})
+	var results []models.GameStats
+	err = cur.All(ctx, &results)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"resulsts": results})
 
 }
 
